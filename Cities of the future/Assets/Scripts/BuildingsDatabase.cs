8958085d2b01e5ce98c4ad88def385f2acd2814b
Lander Aguirre <aguirrelander@opendeusto.es>
2023-04-13 10:26:27 +0200

    Actualizacion

--- Cities of the future/Assets/Scripts/BuildingsDatabase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BuildingsDatabase : MonoBehaviour
-{
-    public List<Building> buildingsDatabase = new List<Building>();
-
-}
